1d82925ced6929243b5b2adc6aac04f2304ce54f
JimmyShi22 <417711026@qq.com>
2020-03-24 18:02:37 +0800

    Download test resource from release

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+cd src/test/resources
+curl -LO https://github.com/WeBankFinTech/WeCross-Fabric-Stub/releases/download/resources/test_resources.tar.gz
+tar -zxvf test_resources.tar.gz
+cd -
+
 ./gradlew verifyGoogleJavaFormat
 ./gradlew build -x test
 ./gradlew test -i
