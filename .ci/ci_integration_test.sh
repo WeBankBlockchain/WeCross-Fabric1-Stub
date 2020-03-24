a9e700c4aed567e63e39d7513ba163d8b0fc8e51
JimmyShi22 <417711026@qq.com>
2020-03-24 20:57:52 +0800

    Add integration test

---
+++ .ci/ci_integration_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash ./scripts/build_fabric_demo_chain.sh
+
+cp -r certs/accounts/*  src/integTest/resources/accounts/
+cp -r certs/stubs/fabric/* src/integTest/resources/stubs/fabric/
+
+./gradlew integTest
