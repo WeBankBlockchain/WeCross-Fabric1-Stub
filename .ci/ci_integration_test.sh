1fd74cc48170721161ffe42c9f07bc61fcc67da6
JimmyShi22 <417711026@qq.com>
2020-03-24 22:30:42 +0800

    use test instead of integration test for simple

--- .ci/ci_integration_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bash ./scripts/build_fabric_demo_chain.sh
-
-cp -r certs/accounts/*  src/integTest/resources/accounts/
-cp -r certs/stubs/fabric/* src/integTest/resources/stubs/fabric/
-
-./gradlew integTest
-./gradlew jacocoTestReport
