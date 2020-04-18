50d90b685a5b042582100704887895eee110d10c
JimmyShi22 <417711026@qq.com>
2020-04-19 01:37:56 +0800

    update check process

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -6,14 +6,12 @@ mkdir -p demo
 cd demo
 bash ../scripts/build_fabric_demo_chain.sh
 
-./gradlew verifyGoogleJavaFormat
-./gradlew build -x test
-
 cp -r certs/accounts/*  ../src/test/resources/accounts/
 cp -r certs/chains/fabric/* ../src/test/resources/chains/fabric/
 
 cd -
 
-
+./gradlew verifyGoogleJavaFormat
+./gradlew build -x test
 ./gradlew test -i
 ./gradlew jacocoTestReport
