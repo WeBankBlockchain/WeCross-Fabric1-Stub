1adf53a03c23fe930ef7336166d3ef73dc998972
JimmyShi22 <417711026@qq.com>
2020-04-18 23:40:02 +0800

    Add name

--- src/main/java/com/webank/wecross/stub/fabric/FabricStubFactory.java
+++ src/main/java/com/webank/wecross/stub/fabric/FabricStubFactory.java
@@ -40,71 +40,13 @@ public class FabricStubFactory implements StubFactory {
     @Override
     public void generateAccount(String path, String[] args) {
         try {
-            /*
-            // Write private key pem file
-            Security.addProvider(new BouncyCastleProvider());
-            KeyPairGenerator keyPairGenerator =
-                    KeyPairGenerator.getInstance("EC", Security.getProvider("BC"));
-
-            keyPairGenerator.initialize(256);
-            KeyPair keyPair = keyPairGenerator.generateKeyPair();
-            PrivateKey ecPrivateKey = keyPair.getPrivate();
-
-            String keyFile = path + "/account.key";
-            File file = new File(keyFile);
-
-            if (!file.createNewFile()) {
-                logger.error("Key file exists! {}", keyFile);
-                return;
-            }
-
-            PemWriter pemWriter = new PemWriter(new FileWriter(file));
-            try {
-                pemWriter.writeObject(new PemObject("PRIVATE KEY", ecPrivateKey.getEncoded()));
-            } finally {
-                pemWriter.close();
-            }
-
-            // Write cert pem file
-            Instant now = Instant.now();
-            Date notBefore = Date.from(now);
-            Date notAfter = Date.from(now.plus(Duration.ofDays(3650)));
-
-            X500Name x500Name = new X500Name("CN=");
-
-            X509v3CertificateBuilder certificateBuilder =
-                    new JcaX509v3CertificateBuilder(
-                            x500Name,
-                            BigInteger.valueOf(now.toEpochMilli()),
-                            notBefore,
-                            notAfter,
-                            x500Name,
-                            keyPair.getPublic());
-
-            ContentSigner contentSigner =
-                    new JcaContentSignerBuilder("SHA256WITHECDSA").build(keyPair.getPrivate());
-
-            X509Certificate cert =
-                    new JcaX509CertificateConverter()
-                            .setProvider(Security.getProvider("BC"))
-                            .getCertificate(certificateBuilder.build(contentSigner));
-
-            String certFile = path + "/account.crt";
-            file = new File(certFile);
-
-            pemWriter = new PemWriter(new FileWriter(file));
-            try {
-                pemWriter.writeObject(new PemObject("CERTIFICATE", cert.getEncoded()));
-            } finally {
-                pemWriter.close();
-            }
-            */
+            // Generate config file only, copy user cert from crypto-config/
 
             // Write config file
             String accountTemplate =
                     "[account]\n"
                             + "    type = 'Fabric1.4'\n"
-                            + "    mspid = ''\n"
+                            + "    mspid = 'Org1MSP'\n"
                             + "    keystore = 'account.key'\n"
                             + "    signcert = 'account.crt'";
 
@@ -130,9 +72,13 @@ public class FabricStubFactory implements StubFactory {
     @Override
     public void generateConnection(String path, String[] args) {
         try {
+            String chainName = new File(path).getName();
+
             String accountTemplate =
                     "[common]\n"
-                            + "    stub = 'fabric'\n"
+                            + "    name = '"
+                            + chainName
+                            + "'\n"
                             + "    type = 'Fabric1.4'\n"
                             + "\n"
                             + "[fabricServices]\n"
@@ -141,15 +87,15 @@ public class FabricStubFactory implements StubFactory {
                             + "    mspId = 'Org1MSP'\n"
                             + "    orgUserName = 'fabric_admin'\n"
                             + "    orgUserAccountPath = 'classpath:accounts/fabric_admin'\n"
-                            + "    ordererTlsCaFile = 'classpath:/stubs/fabric/orderer-tlsca.crt'\n"
+                            + "    ordererTlsCaFile = 'orderer-tlsca.crt'\n"
                             + "    ordererAddress = 'grpcs://localhost:7050'\n"
                             + "\n"
                             + "[peers]\n"
                             + "    [peers.org1]\n"
-                            + "        peerTlsCaFile = 'classpath:/stubs/fabric/org1-tlsca.crt'\n"
+                            + "        peerTlsCaFile = 'org1-tlsca.crt'\n"
                             + "        peerAddress = 'grpcs://localhost:7051'\n"
                             + "    [peers.org2]\n"
-                            + "         peerTlsCaFile = 'classpath:/stubs/fabric/org2-tlsca.crt'\n"
+                            + "         peerTlsCaFile = 'org2-tlsca.crt'\n"
                             + "         peerAddress = 'grpcs://localhost:9051'\n"
                             + "\n"
                             + "# resources is a list\n"
@@ -159,13 +105,7 @@ public class FabricStubFactory implements StubFactory {
                             + "    type = 'FABRIC_CONTRACT'\n"
                             + "    chainCodeName = 'mycc'\n"
                             + "    chainLanguage = \"go\"\n"
-                            + "    peers=['org1','org2']\n"
-                            + "[[resources]]\n"
-                            + "    name = 'HelloWorld'\n"
-                            + "    type = 'FABRIC_CONTRACT'\n"
-                            + "    chainCodeName = 'mygg'\n"
-                            + "    chainLanguage = \"go\"\n"
-                            + "    peers=['org1','org2']";
+                            + "    peers=['org1','org2']\n";
             String confFilePath = path + "/stub.toml";
             File confFile = new File(confFilePath);
             if (!confFile.createNewFile()) {
