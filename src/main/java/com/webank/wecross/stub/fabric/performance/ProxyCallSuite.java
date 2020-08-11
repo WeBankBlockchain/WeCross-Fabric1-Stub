e8a8238c783c5b649b65e78fa65f3a18442bf8db
JimmyShi22 <417711026@qq.com>
2020-08-11 12:33:55 +0800

    Add proxy performance test

--- src/main/java/com/webank/wecross/stub/fabric/performance/ProxyCallSuite.java
+++ src/main/java/com/webank/wecross/stub/fabric/performance/ProxyCallSuite.java
@@ -1,8 +1,10 @@
 package com.webank.wecross.stub.fabric.performance;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.webank.wecross.stub.fabric.EndorsementPolicyAnalyzer;
 import com.webank.wecross.stub.fabric.FabricConnection;
 import com.webank.wecross.stub.fabric.FabricStubFactory;
+import com.webank.wecross.stub.fabric.proxy.ProxyChaincodeResource;
 import java.util.Collection;
 import org.hyperledger.fabric.sdk.ChaincodeID;
 import org.hyperledger.fabric.sdk.Channel;
@@ -11,38 +13,45 @@ import org.hyperledger.fabric.sdk.Peer;
 import org.hyperledger.fabric.sdk.ProposalResponse;
 import org.hyperledger.fabric.sdk.QueryByChaincodeRequest;
 
-public class PureFabricCallSuite implements PerformanceSuite {
+public class ProxyCallSuite implements PerformanceSuite {
+    private final String proxyName = ProxyChaincodeResource.DEFAULT_NAME;
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
     private Channel channel;
     private HFClient hfClient;
     private Collection<Peer> peers;
 
-    public PureFabricCallSuite(String chainPath) throws Exception {
+    public ProxyCallSuite(String chainPath) throws Exception {
         FabricStubFactory fabricStubFactory = new FabricStubFactory();
         FabricConnection fabricConnection =
                 (FabricConnection) fabricStubFactory.newConnection(chainPath);
 
         this.channel = fabricConnection.getChannel();
 
+        if (!fabricConnection.getChaincodeMap().containsKey(proxyName)) {
+            throw new Exception("WeCross proxy chaincode has not been deployed");
+        }
+
         if (!fabricConnection.getChaincodeMap().containsKey("sacc")) {
-            throw new Exception("Resource sacc has not been deployed!");
+            throw new Exception("Resource sacc has not been deployed");
         }
 
         this.hfClient = fabricConnection.getHfClient();
 
         this.peers = fabricConnection.getChaincodeMap().get("sacc").getEndorsers();
 
-        queryOnce();
+        queryOnceByProxy();
     }
 
     @Override
     public String getName() {
-        return "Pure Fabric Call Suite";
+        return "Proxy Call Performance Test Suite";
     }
 
     @Override
     public void call(PerformanceSuiteCallback callback) {
         try {
-            Collection<ProposalResponse> responses = queryOnce();
+            Collection<ProposalResponse> responses = queryOnceByProxy();
             EndorsementPolicyAnalyzer analyzer = new EndorsementPolicyAnalyzer(responses);
 
             if (analyzer.allSuccess()) {
@@ -56,16 +65,34 @@ public class PureFabricCallSuite implements PerformanceSuite {
         }
     }
 
-    private Collection<ProposalResponse> queryOnce() throws Exception {
+    private Collection<ProposalResponse> queryOnceByProxy() throws Exception {
         QueryByChaincodeRequest request = hfClient.newQueryProposalRequest();
-        String cc = "sacc";
+        String cc = proxyName;
         ChaincodeID ccid = ChaincodeID.newBuilder().setName(cc).build();
         request.setChaincodeID(ccid);
-        request.setFcn("query");
-        request.setArgs("a");
-        request.setProposalWaitTime(3000);
+        request.setFcn("constantCall");
+        request.setArgs(buildQueryProxyArgs());
+        request.setProposalWaitTime(5000);
 
         Collection<ProposalResponse> responses = channel.queryByChaincode(request, peers);
         return responses;
+    }
+
+    private String[] buildQueryProxyArgs() throws Exception {
+        class ChaincodeArgs {
+            public String[] args;
+
+            public ChaincodeArgs() {}
+
+            public ChaincodeArgs(String[] args) {
+                this.args = args;
+            }
+        }
+
+        String[] chaincodeArgs = {"a"};
+        String argsJsonString = objectMapper.writeValueAsString(new ChaincodeArgs(chaincodeArgs));
+        String[] args = {"0", "payment.fabric.sacc", "query", argsJsonString};
+
+        return args;
     }
 }
